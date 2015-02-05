beb934b094b993556acba52331d1bf4869a76521
Melissa McWilliams <mmcwilliams@hidglobal.com>
2015-02-05 13:16:01 -0600

    adding android build update and update to product slide

--- platforms/android/assets/www/js/product.js
+++ platforms/android/assets/www/js/product.js
@@ -9,10 +9,11 @@ $(document).ready(function() {
       //run through json array and dump all the elements for the slider
       $.each( results, function( key, value ) {
         if( value['vertical'] == $.cookie('verticalcategory') ){
-          $('.swiper-wrapper').append('<div class="swiper-slide"> <div class="product-slide"> <h1>'+ value['name'] +'</h1> <img src="'+ value['imagefilename'] +'" alt="" /> <p>'+ value['description'] +'</p> <div class="datasheet-download-btn"><a href="'+ value['pdf'] +'" title="">Share Datasheet</a></div></div></div>'
+          $('.swiper-wrapper').append('<div class="swiper-slide"><div class="product-slide"><div class="blue-box-product"> <h1>'+ value['name'] +'</h1> <img src="'+ value['imagefilename'] +'" alt="" /> <p>'+ value['description'] +'</p> <div class="datasheet-download-btn"><a href="'+ value['pdf'] +'" title="">Share Datasheet</a></div></div></div>'
           );
         }
       });
+
       //once we've run through the csv entries and added them to the dom, finally call and enable the swiper
       window.mySwiper = $('.product-slider').swiper({
         mode:'horizontal',
